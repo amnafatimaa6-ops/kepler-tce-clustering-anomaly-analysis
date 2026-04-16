99c3b5cbd9990d05a7c7949a63740653b389f72e
Amna Fatima <amnafatimaa6@gmail.com>
2026-04-16 09:17:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 from model import generate_galaxy
 from universe import render_universe
 
-st.set_page_config(page_title="ExoGalaxy Explorer", layout="wide")
+st.set_page_config(layout="wide")
 
-st.title("🌌 ExoGalaxy 3D Universe Explorer")
+st.title("🌌 ExoGalaxy Explorer")
 
-df = generate_galaxy(800)  # keep smaller for stability
+df = generate_galaxy(600)  # IMPORTANT: keep moderate size
 
 html = render_universe(df)
 
-st.components.v1.html(html, height=850, scrolling=False)
+st.components.v1.html(html, height=900, scrolling=False)
