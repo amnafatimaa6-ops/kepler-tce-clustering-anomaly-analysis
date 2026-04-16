c2a190ac61d9ac13cb2049f5af282310c18c2563
Amna Fatima <amnafatimaa6@gmail.com>
2026-04-16 08:59:17 -0700

    Update universe.py

--- universe.py
+++ universe.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import os
 
 def render_universe():
-    with open("universe_html.txt", "r", encoding="utf-8") as f:
+
+    file_path = "universe_html.txt"
+
+    if not os.path.exists(file_path):
+        st.error("❌ universe_html.txt not found in repo")
+        st.stop()
+
+    with open(file_path, "r", encoding="utf-8") as f:
         html = f.read()
 
     components.html(html, height=900, scrolling=False)
