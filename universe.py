844b2173d25a3f4d99f48c9768a3d2a12e30a89e
Amna Fatima <amnafatimaa6@gmail.com>
2026-04-16 08:58:16 -0700

    Update universe.py

--- universe.py
+++ universe.py
@@ -2,13 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 def render_universe():
-    # Open the 3D universe HTML file
-    with open("static/index.html", "r", encoding="utf-8") as f:
+    with open("universe_html.txt", "r", encoding="utf-8") as f:
         html = f.read()
 
-    # Render inside Streamlit
-    components.html(
-        html,
-        height=900,
-        scrolling=False
-    )
+    components.html(html, height=900, scrolling=False)
